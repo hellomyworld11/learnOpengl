6094a9330843846de072bb8b2f73cdd8ea1a50cf
xupan <2495526723@qq.com>
2024-01-04 17:54:18 +0800

    add files

--- minirenderer/main.cpp
+++ minirenderer/main.cpp
@@ -1,44 +1,25 @@
 #include "tgaimage.h"
-
+#include <iostream>
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
 
+//Bresenham¡¯s algorithm 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-	bool steep = false;
-	if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
-		std::swap(x0, y0);
-		std::swap(x1, y1);
-		steep = true;
-	}
-	if (x0 > x1) {
-		std::swap(x0, x1);
-		std::swap(y0, y1);
-	}
-	int dx = x1 - x0;
-	int dy = y1 - y0;
-	int derror2 = std::abs(dy) * 2;
-	int error2 = 0;
-	int y = y0;
-	for (int x = x0; x <= x1; x++) {
-		if (steep) {
-			image.set(y, x, color);
-		}
-		else {
-			image.set(x, y, color);
-		}
-		error2 += derror2;
-		if (error2 > dx) {
-			y += (y1 > y0 ? 1 : -1);
-			error2 -= dx * 2;
-		}
+	for (float t = 0.; t < 1.; t += .1) {
+		int x = x0 + (x1 - x0)*t;
+		int y = y0 + (y1 - y0)*t;
+		std::cout << "t: " << t <<" x: " << x << " y: " << y << std::endl;
+		image.set(x, y, color);
 	}
 }
 
 int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
-	
+	TGAImage image(40, 40, TGAImage::RGB);
+	line(2, 2, 8, 8, image, white);
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
+
+	getchar();
 	return 0;
 }
