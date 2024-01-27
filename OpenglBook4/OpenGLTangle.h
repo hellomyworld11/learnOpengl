38190c89d188f6c8d0a602d670b5e5987bbcc458
xupan <2495526723@qq.com>
2024-01-27 17:58:15 +0800

    add

---
+++ OpenglBook4/OpenGLTangle.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+#include <glad/glad.h>   //用来方便调用opengl的函数
+#include <GLFW/glfw3.h> //方便开发
+#include <iostream>
+
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+static void processInput(GLFWwindow *window);
+
+
+//2.OpenGL绘制三角形
+int openglTangle_main();
---
+++ OpenglBook4/OpenGlWidget.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <glad/glad.h>   //用来方便调用opengl的函数
+#include <GLFW/glfw3.h> //方便开发
+#include <iostream>
+
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+static void processInput(GLFWwindow *window);
+
+
+//1.OpenGL创建窗口
+int openglWidet_main();
