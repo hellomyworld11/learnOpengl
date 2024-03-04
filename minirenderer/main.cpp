7f0406e3d8ad77599e0c05c2700dc4fdba391356
xupan <2495526723@qq.com>
2024-03-04 16:58:27 +0800

    add

--- minirenderer/main.cpp
+++ minirenderer/main.cpp
@@ -1,82 +1,68 @@
-#include <iostream>
 #include <vector>
-#include <cmath>
+#include <iostream>
+#include <algorithm>
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
+#include "our_gl.h"
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
 Model *model = NULL;
 const int width = 800;
 const int height = 800;
 
-//Bresenham’s algorithm 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-	for (float t = 0.; t < 1.; t += .01) {
-		int x = x0 + (x1 - x0)*t;
-		int y = y0 + (y1 - y0)*t;
-		std::cout << "t: " << t <<" x: " << x << " y: " << y << std::endl;
-		image.set(x, y, color);
-	}
-}
+Vec3f light_dir(1, 1, 1);
+Vec3f       eye(0, -1, 3);
+Vec3f    center(0, 0, 0);
+Vec3f        up(0, 1, 0);
 
-void lineErr(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-	for (int x = x0; x <= x1; x++) {
-		float t = (x - x0) / (float)(x1 - x0);
-		int y = y0*(1. - t) + y1*t;
-		image.set(x, y, color);
-	}
-}
-
-void line1(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-	bool steep = false;
-	if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
-		std::swap(x0, y0);
-		std::swap(x1, y1);
-		steep = true;
-	}
-	if (x0 > x1) {
-		std::swap(x0, x1);
-		std::swap(y0, y1);
+struct GouraudShader : public IShader {
+	Vec3f varying_intensity; // written by vertex shader, read by fragment shader
+	mat<2, 3, float> varying_uv;        // same as above
+	virtual Vec4f vertex(int iface, int nthvert) {
+		varying_uv.set_col(nthvert, model->uv(iface, nthvert));
+		varying_intensity[nthvert] = std::max(0.f, model->normal(iface, nthvert)*light_dir); // get diffuse lighting intensity
+		Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
+		return Viewport*Projection*ModelView*gl_Vertex; // transform it to screen coordinates
 	}
 
-	for (int x = x0; x <= x1; x++) {
-		float t = (x - x0) / (float)(x1 - x0);
-		int y = y0*(1. - t) + y1*t;
-		if (steep) {
-			image.set(y, x, color);
-		}
-		else {
-			image.set(x, y, color);
-		}
+	virtual bool fragment(Vec3f bar, TGAColor &color) {
+		float intensity = varying_intensity*bar;   // interpolate intensity for the current pixel
+		Vec2f uv = varying_uv*bar;                 // interpolate uv for the current pixel
+		color = model->diffuse(uv)*intensity;      // well duh
+		return false;                            // no, we do not discard this pixel
 	}
-}
+};
 
 int main(int argc, char** argv) {
-	if (2 == argc) {
-		model = new Model(argv[1]);
-	}
-	else {
-		model = new Model("obj/african_head.obj");
-	}
+
+	//加载 obj模型 获取 顶点坐标 法向 uv 面
+	model = new Model("obj/african_head.obj");
+	
+	//相机变换
+	lookat(eye, center, up);
+	//视口变换
+	viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
+	//投影变换
+	projection(-1.f / (eye - center).norm());
+	light_dir.normalize();
 
 	TGAImage image(width, height, TGAImage::RGB);
+	TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
+
+	GouraudShader shader;
 	for (int i = 0; i < model->nfaces(); i++) {
-		std::vector<int> face = model->face(i);
+		Vec4f screen_coords[3];
 		for (int j = 0; j < 3; j++) {
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j + 1) % 3]);
-			int x0 = (v0.x + 1.)*width / 2.;
-			int y0 = (v0.y + 1.)*height / 2.;
-			int x1 = (v1.x + 1.)*width / 2.;
-			int y1 = (v1.y + 1.)*height / 2.;
-			line1(x0, y0, x1, y1, image, white);
+			screen_coords[j] = shader.vertex(i, j);
 		}
+		triangle(screen_coords, shader, image, zbuffer);
 	}
 
-	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	image.flip_vertically(); // to place the origin in the bottom left corner of the image
+	zbuffer.flip_vertically();
 	image.write_tga_file("output.tga");
+	zbuffer.write_tga_file("zbuffer.tga");
+
 	delete model;
 	return 0;
 }
